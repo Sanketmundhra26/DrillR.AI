ba5f4cc571807bc594e71f97cf9dc29a04ad8a08
Sanketmundhra <sanketmundhra@gmail.com>
2025-05-02 22:26:20 +0530

    implemented the feedback functionality along with the UI

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-    eslint:{
+    eslint: {
         ignoreDuringBuilds: true,
     },
-    typescript:{
+    typescript: {
         ignoreBuildErrors: true,
-    }
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                child_process: false,
+                fs: false,
+                os: false,
+            };
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
